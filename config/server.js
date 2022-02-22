b0da08f2f3b6def36851eb2366c076c098083076
Gustavo Dantas Galote <gusgalote@outlook.com>
2022-02-22 12:12:05 -0300

    first commit - project structure

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b23d7da124d4a8e981576af220865cc3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ca94a35-fc26-408c-8be6-30deea67027a'
+};
